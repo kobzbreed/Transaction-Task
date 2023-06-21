e59f7569a859c2a85d6c02357756e0e247d6e118
kobzbreed <jitekobimowe10@gmail.com>
2023-06-21 22:17:50 +0100

    next attempt

--- src/duplicate-transactions.js
+++ src/duplicate-transactions.js
@@ -55,61 +55,61 @@ function findDuplicateTransactions(transactions) {
     return allDuplicates
 }
 
-// export default findDuplicateTransactions;
+export default findDuplicateTransactions;
 
 
-const input = [
-    {
-      id: 3,
-      sourceAccount: 'A',
-      targetAccount: 'B',
-      amount: 100,
-      category: 'eating_out',
-      time: '2018-03-02T10:34:30.000Z',
-    },
-    {
-      id: 1,
-      sourceAccount: 'A',
-      targetAccount: 'B',
-      amount: 100,
-      category: 'eating_out',
-      time: '2018-03-02T10:33:00.000Z',
-    },
-    {
-      id: 6,
-      sourceAccount: 'A',
-      targetAccount: 'C',
-      amount: 250,
-      category: 'other',
-      time: '2018-03-02T10:33:05.000Z',
-    },
-    {
-      id: 4,
-      sourceAccount: 'A',
-      targetAccount: 'B',
-      amount: 100,
-      category: 'eating_out',
-      time: '2018-03-02T10:36:00.000Z',
-    },
-    {
-      id: 2,
-      sourceAccount: 'A',
-      targetAccount: 'B',
-      amount: 100,
-      category: 'eating_out',
-      time: '2018-03-02T10:33:50.000Z',
-    },
-    {
-      id: 5,
-      sourceAccount: 'A',
-      targetAccount: 'C',
-      amount: 250,
-      category: 'other',
-      time: '2018-03-02T10:33:00.000Z',
-    },
-  ];
+// const input = [
+//     {
+//       id: 3,
+//       sourceAccount: 'A',
+//       targetAccount: 'B',
+//       amount: 100,
+//       category: 'eating_out',
+//       time: '2018-03-02T10:34:30.000Z',
+//     },
+//     {
+//       id: 1,
+//       sourceAccount: 'A',
+//       targetAccount: 'B',
+//       amount: 100,
+//       category: 'eating_out',
+//       time: '2018-03-02T10:33:00.000Z',
+//     },
+//     {
+//       id: 6,
+//       sourceAccount: 'A',
+//       targetAccount: 'C',
+//       amount: 250,
+//       category: 'other',
+//       time: '2018-03-02T10:33:05.000Z',
+//     },
+//     {
+//       id: 4,
+//       sourceAccount: 'A',
+//       targetAccount: 'B',
+//       amount: 100,
+//       category: 'eating_out',
+//       time: '2018-03-02T10:36:00.000Z',
+//     },
+//     {
+//       id: 2,
+//       sourceAccount: 'A',
+//       targetAccount: 'B',
+//       amount: 100,
+//       category: 'eating_out',
+//       time: '2018-03-02T10:33:50.000Z',
+//     },
+//     {
+//       id: 5,
+//       sourceAccount: 'A',
+//       targetAccount: 'C',
+//       amount: 250,
+//       category: 'other',
+//       time: '2018-03-02T10:33:00.000Z',
+//     },
+//   ];
 
-console.log(findDuplicateTransactions(input))
+// console.log(findDuplicateTransactions(input))
 
 
 
